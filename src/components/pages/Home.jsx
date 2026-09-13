b503b031775a726bd1ecbb3dbc4315142cab1945
Murad51 <mhuseynov756@gmail.com>
2026-09-13 23:56:54 +0400

    feat: Add cart functionality and product page navigation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -29,6 +29,16 @@ function Home({ onAddToCart }) {
 
     if (!imgPath) return "";
 
+    if (imgPath.startsWith("/api/images")) {
+      imgPath = imgPath.replace("/api/images", "/images");
+    }
+
+    if (imgPath.startsWith("http")) return imgPath;
+
+    if (!imgPath.startsWith("/")) {
+      imgPath = "/" + imgPath;
+    }
+
     return `${API_BASE_URL}${imgPath}`;
   };
 
